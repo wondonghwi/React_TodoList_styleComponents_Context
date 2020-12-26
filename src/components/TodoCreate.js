6109c7e5d62fa4e729a77e87e301d4c0586b498d
donghwi <ehdehd1289@naver.com>
2020-12-27 02:36:10 +0900

    Todo -> View develop Compleate

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -48,15 +48,50 @@ const CircleButton = styled.button`
   `}
 `;
 
+const InsertFormPositioner = styled.div`
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+`;
+
+const InsertForm = styled.form`
+  background: #f8f9fa;
+  padding: 32px 32px 72px 32px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+  border-top: 1px solid #e9ecef;
+`;
+
+const Input = styled.input`
+  padding: 12px;
+  border-radius: 4px;
+  border: 1px solid #dee2e6;
+  width: 100%;
+  outline: none;
+  font-size: 18px;
+  box-sizing: border-box;
+`;
+
+
 const TodoCreate = () => {
 
   const [open, setOpen] = useState(false);
   const onToggle = () => setOpen(!open);
 
   return (
-    <CircleButton onClick={onToggle} open={open}>
-      <MdAdd/>
-    </CircleButton>
+    <>
+      {open && (
+        <InsertFormPositioner>
+          <InsertForm>
+            <Input placeholder='할 일을 입력 후, Enter를 눌러주세요.' autoFocus/>
+          </InsertForm>
+        </InsertFormPositioner>
+      )}
+      <CircleButton onClick={onToggle} open={open}>
+        <MdAdd/>
+      </CircleButton>
+    </>
   );
 };
 
